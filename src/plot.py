89261eb41cd4b5804309e850f5f86bed3bba87b4
irvin-s <isb@cin.ufpe.br>
2020-11-23 19:12:19 +0000

    Atualizando

--- src/plot.py
+++ src/plot.py
@@ -4,9 +4,19 @@ import matplotlib.pyplot as plt
 
 print ("Plot from dataset")
 
-plt.style.use("bmh")
+plt.style.use('ggplot')
 
-df = pd.read_csv('../data/crx.data', sep=",")
-df.columns
-df.head()
-df.info()
+df = pd.read_csv('../data/crx.data', sep=',')
+#df.columns
+#df.head()
+#df.info()
+
+
+#Histograma 1
+#df.columns
+#df.head()
+#df.info()
+df.groupby('a16')['a16'].count()
+
+
+#Histograma 2
